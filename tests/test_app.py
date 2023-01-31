30427c209ee5251df08747c9479b404dfe1d2f7d
Sabrina <sabrina.samuel@madetech.com>
2023-01-31 11:35:07 +0000

    Add event name field

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
 from app import app
 
-def test_index():
+def test_index_heading():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, World!" in response.data
+    assert b"Events" in response.data
+    assert b"<h1>" in response.data
     
+def test_index_text_field():
+    response = app.test_client().get('/')
+    assert b"<input" in response.data
+    assert b"Event name" in response.data
+    assert b"Event name</label>" in response.data
