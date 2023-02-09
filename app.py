c9428fd13cfb0792f76fca6621d9d3ce1f4b2627
Sabrina <sabrina.samuel@madetech.com>
2023-02-09 17:08:41 +0000

    add back functionality to add and delete all

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, redirect, render_template, request
 from datetime import date
 import uuid
 import json
-# from localStoragePy import localStoragePy
-# import os
+from localStoragePy import localStoragePy
+import os
 
-# localStorage = localStoragePy("cs50-todo", "text")
+localStorage = localStoragePy("cs50-todo", "text")
                                           
 app = Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.after_request
 def after_request(response):
@@ -18,47 +19,49 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.context_processor
 def inject_year():
     today = date.today()
     yearNow = today.strftime("%Y")
     return dict(year="2022 - {}".format(yearNow))
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-    # if request.method == "POST" and "submit-event" in request.form:
-    #     previousEvents = json.loads(localStorage.getItem("list")) or []
+    if request.method == "POST" and "submit-event" in request.form:
+        previousEvents = json.loads(localStorage.getItem("list")) or []
         
-    #     id = uuid.uuid4().int
-    #     newEvent = {"id": id, "name": request.form.get("new-event")}
-    #     if previousEvents is None or previousEvents == []:
-    #         localStorage.setItem("list", json.dumps([newEvent]))
-    #         events = json.loads(localStorage.getItem("list"))
-    #         return render_template("index.html", eventLists=events)
-    #     else:
-    #         previousEvents.extend([newEvent])
-    #         localStorage.setItem("list", json.dumps(previousEvents))
-    #         events = json.loads(localStorage.getItem("list"))
-    #         return render_template("index.html", eventLists=previousEvents)
-    # elif request.method == "POST" and "delete-all" in request.form:
-    #     localStorage.clear()
-    #     localStorage.setItem("list", '[]')
-    #     return render_template("index.html")
-    # else:
-    #     events = localStorage.getItem("list")
-    #     if events is None:
-    #         localStorage.setItem("list", '[]')
-    #         return render_template("index.html")
-    #     else:
-    #         events = json.loads(localStorage.getItem("list"))
-    #         return render_template("index.html", eventLists=events)
+        id = uuid.uuid4().int
+        newEvent = {"id": id, "name": request.form.get("new-event")}
+        if previousEvents is None or previousEvents == []:
+            localStorage.setItem("list", json.dumps([newEvent]))
+            events = json.loads(localStorage.getItem("list"))
+            return render_template("index.html", eventLists=events)
+        else:
+            previousEvents.extend([newEvent])
+            localStorage.setItem("list", json.dumps(previousEvents))
+            events = json.loads(localStorage.getItem("list"))
+            return render_template("index.html", eventLists=previousEvents)
+    elif request.method == "POST" and "delete-all" in request.form:
+        localStorage.clear()
+        localStorage.setItem("list", '[]')
+        return render_template("index.html")
+    else:
+        events = localStorage.getItem("list")
+        if events is None:
+            localStorage.setItem("list", '[]')
+            return render_template("index.html")
+        else:
+            events = json.loads(localStorage.getItem("list"))
+            return render_template("index.html", eventLists=events)
+
 
 @app.route("/error")
 def error():
     return render_template("error.html")
 
-if __name__ == "__main__": 
-    app.run(debug=False, host='0.0.0.0')
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(debug=True, host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
+
