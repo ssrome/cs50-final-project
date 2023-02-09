a9d046f0f1ddbb3219b4b674cd289b1ea8de14b1
Sabrina <sabrina.samuel@madetech.com>
2023-02-09 17:18:39 +0000

    Fix variable naming issues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request
 from datetime import date
 import uuid
 import json
@@ -23,26 +23,25 @@ def after_request(response):
 @app.context_processor
 def inject_year():
     today = date.today()
-    yearNow = today.strftime("%Y")
-    return dict(year="2022 - {}".format(yearNow))
+    year_now = today.strftime("%Y")
+    return dict(year="2022 - {}".format(year_now))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST" and "submit-event" in request.form:
-        previousEvents = json.loads(localStorage.getItem("list")) or []
+        previous_events = json.loads(localStorage.getItem("list")) or []
         
-        id = uuid.uuid4().int
-        newEvent = {"id": id, "name": request.form.get("new-event")}
-        if previousEvents is None or previousEvents == []:
-            localStorage.setItem("list", json.dumps([newEvent]))
+        event_id = uuid.uuid4().int
+        new_event = {"id": event_id, "name": request.form.get("new-event")}
+        if previous_events is None or previous_events == []:
+            localStorage.setItem("list", json.dumps([new_event]))
             events = json.loads(localStorage.getItem("list"))
             return render_template("index.html", eventLists=events)
         else:
-            previousEvents.extend([newEvent])
-            localStorage.setItem("list", json.dumps(previousEvents))
-            events = json.loads(localStorage.getItem("list"))
-            return render_template("index.html", eventLists=previousEvents)
+            previous_events.extend([new_event])
+            localStorage.setItem("list", json.dumps(previous_events))
+            return render_template("index.html", eventLists=previous_events)
     elif request.method == "POST" and "delete-all" in request.form:
         localStorage.clear()
         localStorage.setItem("list", '[]')
@@ -64,4 +63,3 @@ def error():
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
-
