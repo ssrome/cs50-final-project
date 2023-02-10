03b7a79199a3537afc0ed9306f92172b9997033a
Sabrina <sabrina.samuel@madetech.com>
2023-02-10 16:12:45 +0000

    Remove unused code from inject_year

--- src/inject_year.py
+++ src/inject_year.py
@@ -5,7 +5,5 @@ class InjectYear():
     def inject_year(self):
         today = date.today()
         year_now = today.strftime("%Y")
-        print(year_now)
-        # return dict(year="2022 - {}".format(year_now))
         return f'2022 - {year_now}'
 
