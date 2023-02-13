286df2e31c5ccd1a3af3bd9193202844cd40d87e
Sabrina <ssamuelrome@gmail.com>
2023-02-13 01:13:45 +0000

    Change to static methods in Delete class

--- src/delete.py
+++ src/delete.py
@@ -1,11 +1,13 @@
 class Delete:
-    def delete_item(self, event_list, index=None):
+    @staticmethod
+    def delete_item(event_list, index=None):
         if len(event_list) > 0 and index:
             del event_list[index]
             return event_list
         else:
             return event_list
 
-    def delete_all(self, event_list):
+    @staticmethod
+    def delete_all(event_list):
         event_list.clear()
         return event_list
