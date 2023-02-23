ff184fd1e95356d88685618c9d4c04774532c001
Sabrina <sabrina.samuel@madetech.com>
2023-02-23 14:27:19 +0000

    Add item_list variables into own file for tests

---
+++ tests/fixtures/item_list_response.py
@@ -0,0 +1,38 @@
+ALL_FALSE = [
+    {
+        "id": 1,
+        "is_complete": False,
+        "is_edit": False
+    },
+    {
+        "id": 2,
+        "is_complete": False,
+        "is_edit": False
+    }
+]
+
+ONE_COMPLETE_TRUE = [
+    {
+        "id": 1,
+        "is_complete": True,
+        "is_edit": False
+    },
+    {
+        "id": 2,
+        "is_complete": False,
+        "is_edit": False
+    }
+]
+
+ONE_EDIT_TRUE = [
+    {
+        "id": 1,
+        "is_complete": False,
+        "is_edit": True
+    },
+    {
+        "id": 2,
+        "is_complete": False,
+        "is_edit": False
+    }
+]
