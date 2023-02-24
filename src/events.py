6f53e4a36e1019c56a68b04a5bbe784ec8774e09
Sabrina <sabrina.samuel@madetech.com>
2023-02-24 11:28:45 +0000

    Events class to call delete all method

--- src/events.py
+++ src/events.py
@@ -1,3 +1,4 @@
+from src.delete import Delete
 from src.update_complete_status import UpdateCompleteStatus
 from src.update_edit_status import UpdateEditStatus
 
@@ -6,19 +7,23 @@ class Events:
     def __init__(self):
         self.update_complete_status = UpdateCompleteStatus()
         self.update_edit_status = UpdateEditStatus()
+        self.delete = Delete()
 
-    def __call__(self, events, index, method, form):
+    def __call__(self, item_list, index, method, form):
         if method == "POST":
             if "complete-event" in form:
-                response = self.update_complete_status(events, "complete-event", index)
+                response = self.update_complete_status(item_list, "complete-event", index)
                 return response
             elif "incomplete-event" in form:
-                response = self.update_complete_status(events, "incomplete-event", index)
+                response = self.update_complete_status(item_list, "incomplete-event", index)
                 return response
             elif "edit-event" in form:
-                response = self.update_edit_status(events, "edit-event", index)
+                response = self.update_edit_status(item_list, "edit-event", index)
                 return response
             elif "save-event" in form:
-                response = self.update_edit_status(events, "save-event", index)
+                response = self.update_edit_status(item_list, "save-event", index)
                 return response
-        return events
+            elif "delete-event" in form:
+                response = self.delete.delete_item(item_list, index)
+                return response
+        return item_list
--- tests/test_events.py
+++ tests/test_events.py
@@ -1,9 +1,20 @@
 from unittest.mock import MagicMock
 from src.events import Events
-from tests.fixtures.item_list_response import ALL_FALSE, ONE_COMPLETE_TRUE, ONE_EDIT_TRUE
+from tests.fixtures.item_list_response import ONE_COMPLETE_TRUE, ONE_EDIT_TRUE
 import pytest
 
-ITEM_LIST = ALL_FALSE
+ITEM_LIST = [
+    {
+        "id": 1,
+        "is_complete": False,
+        "is_edit": False
+    },
+    {
+        "id": 2,
+        "is_complete": False,
+        "is_edit": False
+    }
+]
 
 request = MagicMock()
 request.method = "POST"
@@ -11,6 +22,7 @@ request_form_with_complete_event = {"complete-event": "0"}
 request_form_with_incomplete_event = {"incomplete-event": "0"}
 request_form_with_edit_event = {"edit-event": "0"}
 request_form_with_save_event = {"save-event": "0"}
+request_form_with_delete_event = {"delete-event": "0"}
 
 
 @pytest.fixture
@@ -42,3 +54,8 @@ def test_it_calls_update_edit_status_with_edit_event(events):
 def test_it_calls_update_edit_status_with_save_event(events):
     response = events(ONE_EDIT_TRUE, 0, "POST", request_form_with_save_event)
     assert response[0]["is_edit"] is False
+
+
+def test_it_calls_delete_item(events):
+    response = events(ONE_EDIT_TRUE, 0, "POST", request_form_with_delete_event)
+    assert response[0]["id"] != 1
